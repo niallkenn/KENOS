0daa37534a04bc8621841fafec6523c9988b3e96
niallkenn <niallkennedy@gmail.com>
2026-07-29 14:23:37 +0100

    Print the alphabet using conditional loop

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,7 +1,15 @@
-mov ah, 0xe
+mov ah, 0xe                         ; print first character, a
 mov al, 'a'
-INT 0x10
+int 0x10                            
 
-jmp $                           ;jump to current instruction (idk why or what this does)
-times 510-($-$$) db 0           ;fill sector
-db 0x55, 0xaa                   ;add bootloader signature
+print:                              ; prints alphabet using loop,
+    inc al                          ; the loop increment the character until
+    cmp al, 'z' + 1                 ; it is past z, then exits
+    je end
+    int 0x10
+    jmp print
+
+end:
+    jmp $                           ; jump to current instruction (idk why or what this does)
+    times 510-($-$$) db 0           ; fill sector
+    db 0x55, 0xaa                   ; add bootloader signature
