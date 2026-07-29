a912259e0c82b1c19b5f0608492da39c277dfa98
niallkenn <niallkennedy@gmail.com>
2026-07-29 14:53:22 +0100

    Add typing using keyboard interrupt

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,20 +1,29 @@
 [org 0x7c00]
 
-string:
-    db "Hello world!", 0
-
 mov ah, 0xe
 mov bx, string
 
 printString:                        ; loop through string and print each char,
     mov al, [bx]                    ; until last char is terminator
     cmp al, 0
-    je end
+    je next
     int 0x10
     inc bx
     jmp printString
 
-end:
-    jmp $                           ; jump to current instruction (idk why or what this does)
-    times 510-($-$$) db 0           ; fill sector
-    db 0x55, 0xaa                   ; add bootloader signature
+next:
+    mov ah, 0
+    int 0x16
+    mov ah, 0xe
+    int 0x10
+    jmp next
+
+string:
+    db "Hello world!", 10, 13, 0
+
+char:
+    db 0
+
+jmp $                           ; jump to current instruction (idk why or what this does)
+times 510-($-$$) db 0           ; fill sector
+db 0x55, 0xaa                   ; add bootloader signature
