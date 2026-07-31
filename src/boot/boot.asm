4792c452d0623729815facd59912a091d2b65109
niallkenn <niallkennedy@gmail.com>
2026-07-31 01:02:05 +0100

    Add print_string_16 func

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -16,5 +16,26 @@
 
     sti                         ; re-enable interrupts
 
+    mov si, REAL_MODE_MSG
+    call print_string_16
+
+    jmp $                       ; keep jmp here (do not excecute functions past here)
+
+print_string_16:
+    pusha
+    mov ah, 0x0e
+    loop:
+        mov al, [si]
+        inc si
+        cmp al, 0
+        je done
+        int 0x10
+        jmp loop
+    done:
+        popa
+        ret
+
+REAL_MODE_MSG: db "Started in 16-bit real mode...", 13, 10, 0
+
 times 510-($-$$) db 0           ; fill sector
 db 0x55, 0xaa                   ; add bootloader signature
