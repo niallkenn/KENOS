eeccb03b7b5f04bef96d1353344197390f2b521b
niallkenn <niallkennedy@gmail.com>
2026-08-26 12:55:05 +0100

    fat16 format bios parameter block, the filesystem structure definition

--- src/drivers/fat16.cpp
+++ src/drivers/fat16.cpp
@@ -24,6 +24,32 @@ bool FAT16::format() {
     buffer[510] = 0x55;
     buffer[511] = 0xAA;
 
+    buffer[0x0B] = 0x00;
+    buffer[0x0C] = 0x02;
+
+    buffer[0x0D] = 4;
+
+    buffer[0x0E] = 4;
+    buffer[0x0F] = 0;
+
+    buffer[0x10] = 2;
+
+    buffer[0x11] = 0x00;
+    buffer[0x12] = 0x02;
+
+    buffer[0x13] = 0;
+    buffer[0x14] = 0;
+
+    buffer[0x15] = 0xF8;
+
+    buffer[0x16] = 128;
+    buffer[0x17] = 0;
+
+    buffer[0x20] = 0x00;
+    buffer[0x21] = 0x00;
+    buffer[0x22] = 0x02;
+    buffer[0x23] = 0x00;
+
     const char* fat16_str = "FAT16   ";
     int index = 0;
     for (int i = 0; i < 8; i++) {
